24b6a814ffc8a134e252a8841676b8e1aaecf670
karaba <adriel@arismunandar.com>
2025-05-31 11:44:08 -0400

    Updated App.js with React code and BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* Define your Route components here */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
